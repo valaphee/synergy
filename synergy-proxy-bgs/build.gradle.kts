78074b541fc9bf364d3f06ef60fb1ecaa8ea8577
Kevin Ludwig <signing@valaphee.com>
2022-04-15 15:43:38 +0200

    Re-add bgs

---
+++ synergy-proxy-bgs/build.gradle.kts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020-2022, GrieferGames, Valaphee.
+ * All rights reserved.
+ */
+
+dependencies {
+    implementation(project(":synergy-api"))
+    implementation("com.google.protobuf:protobuf-kotlin:3.20.0")
+    implementation("com.hubspot.jackson:jackson-datatype-protobuf:0.9.12")
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
+}
